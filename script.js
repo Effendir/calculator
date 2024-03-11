049bdacc4ba9f5f1b1d24884a91fb62748c5c355
eugene dami <eugene.dami@yahoo.fr>
2024-03-11 18:02:45 +0100

    Define calculus functions

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 function add(a, b) {
   return a + b
 }
+
+function substract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function operate(op, numA, numB) {
+  if (op == "+") {add(numA, numB)}
+  else if (op == "-") {substract(numA, numB)}
+  else if (op == "*") {multiply(numA, numB)}
+  else if (op == "/") {divide(numA, numB)}
+}
+
+let numberA;
+let numberB;
+let operator;
+let result;
