1f9d06d9df565704d851ddeb5be1e2d6134618c7
eugene dami <eugene.dami@yahoo.fr>
2024-03-12 10:01:15 +0100

    Define UI

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ function divide(a, b) {
 }
 
 function operate(op, numA, numB) {
-  if (op == "+") {add(numA, numB)}
-  else if (op == "-") {substract(numA, numB)}
-  else if (op == "*") {multiply(numA, numB)}
-  else if (op == "/") {divide(numA, numB)}
+  if (op == "+") {return add(numA, numB)}
+  else if (op == "-") {return substract(numA, numB)}
+  else if (op == "*") {return multiply(numA, numB)}
+  else if (op == "/") {return divide(numA, numB)}
 }
 
 let numberA;
@@ -30,11 +30,31 @@ let addButton = document.querySelector(".add");
 let substractButton = document.querySelector(".substract");
 let multiplyButton = document.querySelector(".multiply");
 let divideButton = document.querySelector(".divide");
+let equalButton = document.querySelector(".equal")
 let resultDisplay = document.querySelector(".result");
 let numAInput = document.querySelector("#numA");
 let numBInput = document.querySelector("#numB");
 let clearButton = document.querySelector(".clear");
 
-addButton.addEventListener((click) => {
-  console.log("hey");
+addButton.addEventListener("click", (e) => {
+  operator = "+";
+});
+
+substractButton.addEventListener("click", (e) => {
+  operator = "-";
+});
+
+multiplyButton.addEventListener("click", (e) => {
+  operator = "*";
+});
+
+divideButton.addEventListener("click", (e) => {
+  operator = "/";
+});
+
+equalButton.addEventListener("click", (e) => {
+  numberA = +numAInput.value;
+  numberB = +numBInput.value;
+  result = operate(operator, numberA, numberB);
+  resultDisplay.innerText = result;
 })
